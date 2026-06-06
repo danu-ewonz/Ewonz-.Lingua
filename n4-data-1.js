03aade6911de3601bece4ce7d07313114b1e8531
danu-ewonz <ewonz2006@gmail.com>
2026-06-07 00:54:26 +0530

    Add new kanji data to n4-data-1.js

---
+++ n4-data-1.js
@@ -0,0 +1,12 @@
+let kanjiN4_1 = [
+{kanji:"会",kana:"かい",romaji:"kai",meaning:"හමුව / Meeting"},
+{kanji:"同",kana:"どう",romaji:"dou",meaning:"එකම / Same"},
+{kanji:"事",kana:"こと",romaji:"koto",meaning:"දේ / Thing"},
+{kanji:"自",kana:"じ",romaji:"ji",meaning:"ස්වයං / Self"},
+{kanji:"社",kana:"しゃ",romaji:"sha",meaning:"සමාගම / Company"},
+{kanji:"行",kana:"こう",romaji:"kou",meaning:"යෑම / Go"},
+{kanji:"見",kana:"けん",romaji:"ken",meaning:"බැලීම / See"},
+{kanji:"月",kana:"げつ",romaji:"getsu",meaning:"මාස / Month"},
+{kanji:"思",kana:"し",romaji:"shi",meaning:"සිතීම / Think"},
+{kanji:"時",kana:"じ",romaji:"ji",meaning:"වේලාව / Time"}
+];
