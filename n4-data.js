3e742a6f9917436bd2fe00ca7da19d2d05e7995a
danu-ewonz <ewonz2006@gmail.com>
2026-06-07 00:58:38 +0530

    Add N4 kanji vocabulary data

---
+++ n4-data.js
@@ -0,0 +1,37 @@
+let kanjiN4_3 = [
+
+{kanji:"食",kana:"しょく",romaji:"shoku",meaning:"කෑම / Food"},
+{kanji:"飲",kana:"いん",romaji:"in",meaning:"බීම / Drink"},
+{kanji:"買",kana:"かい",romaji:"kai",meaning:"මිලදී ගැනීම / Buy"},
+{kanji:"売",kana:"ばい",romaji:"bai",meaning:"විකිණීම / Sell"},
+{kanji:"作",kana:"さく",romaji:"saku",meaning:"සෑදීම / Make"},
+{kanji:"使",kana:"し",romaji:"shi",meaning:"භාවිතය / Use"},
+{kanji:"働",kana:"どう",romaji:"dou",meaning:"වැඩ / Work"},
+
+{kanji:"住",kana:"じゅう",romaji:"juu",meaning:"ජීවත් වීම / Live"},
+{kanji:"待",kana:"たい",romaji:"tai",meaning:"බලා සිටීම / Wait"},
+{kanji:"持",kana:"じ",romaji:"ji",meaning:"අල්ලාගෙන සිටීම / Hold"},
+{kanji:"送",kana:"そう",romaji:"sou",meaning:"යැවීම / Send"},
+{kanji:"取",kana:"しゅ",romaji:"shu",meaning:"ගැනීම / Take"},
+{kanji:"開",kana:"かい",romaji:"kai",meaning:"විවෘත / Open"},
+{kanji:"閉",kana:"へい",romaji:"hei",meaning:"වසා / Close"},
+
+{kanji:"始",kana:"し",romaji:"shi",meaning:"ආරම්භ / Start"},
+{kanji:"終",kana:"しゅう",romaji:"shuu",meaning:"අවසන් / End"},
+{kanji:"知",kana:"ち",romaji:"chi",meaning:"දැනගැනීම / Know"},
+{kanji:"分",kana:"ぶん",romaji:"bun",meaning:"බෙදීම / Divide"},
+{kanji:"考",kana:"こう",romaji:"kou",meaning:"සිතීම / Think"},
+
+{kanji:"言",kana:"げん",romaji:"gen",meaning:"කතා / Say"},
+{kanji:"聞",kana:"ぶん",romaji:"bun",meaning:"අසීම / Hear"},
+{kanji:"見",kana:"けん",romaji:"ken",meaning:"බැලීම / See"},
+{kanji:"読",kana:"どく",romaji:"doku",meaning:"කියවීම / Read"},
+{kanji:"書",kana:"しょ",romaji:"sho",meaning:"ලියීම / Write"},
+
+{kanji:"山",kana:"さん",romaji:"san",meaning:"කන්ද / Mountain"},
+{kanji:"川",kana:"せん",romaji:"sen",meaning:"ගඟ / River"},
+{kanji:"田",kana:"でん",romaji:"den",meaning:"වගාව / Field"},
+{kanji:"森",kana:"しん",romaji:"shin",meaning:"වනාන්තර / Forest"},
+{kanji:"林",kana:"りん",romaji:"rin",meaning:"කැලෑ / Woods"}
+
+];
