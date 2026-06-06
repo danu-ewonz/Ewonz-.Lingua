feb097c20a26805ba70ecda6f8dce37468d299a1
danu-ewonz <ewonz2006@gmail.com>
2026-06-07 00:57:02 +0530

    Add kanji vocabulary data for N4 level

---
+++ n4-data-2.js
@@ -0,0 +1,43 @@
+let kanjiN4_2 = [
+
+{kanji:"友",kana:"とも",romaji:"tomo",meaning:"මිතුර / Friend"},
+{kanji:"父",kana:"ちち",romaji:"chichi",meaning:"තාත්තා / Father"},
+{kanji:"母",kana:"はは",romaji:"haha",meaning:"අම්මා / Mother"},
+{kanji:"兄",kana:"あに",romaji:"ani",meaning:"අයියා / Older brother"},
+{kanji:"姉",kana:"あね",romaji:"ane",meaning:"අක්කා / Older sister"},
+{kanji:"弟",kana:"おとうと",romaji:"otouto",meaning:"කුඩා සහෝදරයා / Younger brother"},
+{kanji:"妹",kana:"いもうと",romaji:"imouto",meaning:"කුඩා සහෝදරිය / Younger sister"},
+
+{kanji:"店",kana:"みせ",romaji:"mise",meaning:"කඩය / Shop"},
+{kanji:"駅",kana:"えき",romaji:"eki",meaning:"දුම්රිය ස්ථානය / Station"},
+{kanji:"道",kana:"みち",romaji:"michi",meaning:"පාර / Road"},
+{kanji:"近",kana:"ちか",romaji:"chika",meaning:"ආසන්න / Near"},
+{kanji:"遠",kana:"とお",romaji:"too",meaning:"දුර / Far"},
+{kanji:"前",kana:"まえ",romaji:"mae",meaning:"ඉදිරිය / Front"},
+{kanji:"後",kana:"あと",romaji:"ato",meaning:"පසුපස / Back"},
+
+{kanji:"上",kana:"うえ",romaji:"ue",meaning:"ඉහළ / Up"},
+{kanji:"下",kana:"した",romaji:"shita",meaning:"පහළ / Down"},
+{kanji:"左",kana:"ひだり",romaji:"hidari",meaning:"වම / Left"},
+{kanji:"右",kana:"みぎ",romaji:"migi",meaning:"දකුණ / Right"},
+{kanji:"中",kana:"なか",romaji:"naka",meaning:"ඇතුළ / Inside"},
+
+{kanji:"外",kana:"そと",romaji:"soto",meaning:"පිටත / Outside"},
+{kanji:"名",kana:"な",romaji:"na",meaning:"නම / Name"},
+{kanji:"空",kana:"そら",romaji:"sora",meaning:"අහස / Sky"},
+{kanji:"海",kana:"うみ",romaji:"umi",meaning:"මුහුද / Sea"},
+{kanji:"花",kana:"はな",romaji:"hana",meaning:"මල් / Flower"},
+
+{kanji:"天",kana:"てん",romaji:"ten",meaning:"අහස / Heaven"},
+{kanji:"気",kana:"き",romaji:"ki",meaning:"ශක්තිය / Spirit"},
+{kanji:"光",kana:"ひかり",romaji:"hikari",meaning:"ආලෝකය / Light"},
+{kanji:"音",kana:"おと",romaji:"oto",meaning:"ශබ්දය / Sound"},
+{kanji:"力",kana:"ちから",romaji:"chikara",meaning:"ශක්තිය / Power"},
+
+{kanji:"心",kana:"こころ",romaji:"kokoro",meaning:"හදවත / Heart"},
+{kanji:"体",kana:"からだ",romaji:"karada",meaning:"ශරීරය / Body"},
+{kanji:"頭",kana:"あたま",romaji:"atama",meaning:"හිස / Head"},
+{kanji:"手",kana:"て",romaji:"te",meaning:"අත / Hand"},
+{kanji:"足",kana:"あし",romaji:"ashi",meaning:"පා / Foot"}
+
+];
