e5a01daf47e6e14613d3a79e9c92426ed8df6f93
danu-ewonz <ewonz2006@gmail.com>
2026-06-07 14:31:34 +0530

    Update n4-data-3.js

--- n4-data-3.js
+++ n4-data-3.js
@@ -1,11 +1,52 @@
-window.kanjiList = window.kanjiList.concat([
-  {k:"山",en:"Mountain",si:"කන්ද",h:"やま",r:"yama"},
-  {k:"川",en:"River",si:"ගඟ",h:"かわ",r:"kawa"},
-  {k:"海",en:"Sea",si:"මුහුද",h:"うみ",r:"umi"},
-  {k:"空",en:"Sky",si:"අහස",h:"そら",r:"sora"},
-  {k:"花",en:"Flower",si:"මල්",h:"はな",r:"hana"},
+window.kanjiList3 = [
+{k:"薬",en:"Medicine",si:"බෙහෙත්",h:"くすり",r:"kusuri"},
+{k:"店",en:"Shop",si:"කඩය",h:"みせ",r:"mise"},
+{k:"村",en:"Village",si:"ගම",h:"むら",r:"mura"},
+{k:"町",en:"Town",si:"නගරය",h:"まち",r:"machi"},
+{k:"社",en:"Company",si:"සමාගම",h:"しゃ",r:"sha"},
 
-  {k:"魚",en:"Fish",si:"මාළු",h:"さかな",r:"sakana"},
-  {k:"犬",en:"Dog",si:"බල්ලා",h:"いぬ",r:"inu"},
-  {k:"鳥",en:"Bird",si:"කුරුල්ලා",h:"とり",r:"tori"}
-]);
+{k:"会",en:"Meet",si:"හමුව",h:"かい",r:"kai"},
+{k:"同",en:"Same",si:"එකම",h:"どう",r:"dou"},
+{k:"事",en:"Thing",si:"දේ",h:"こと",r:"koto"},
+{k:"自",en:"Self",si:"තමන්",h:"じ",r:"ji"},
+{k:"動",en:"Move",si:"චලනය",h:"どう",r:"dou"},
+
+{k:"発",en:"Departure",si:"පිටවීම",h:"はつ",r:"hatsu"},
+{k:"着",en:"Arrive",si:"පැමිණීම",h:"ちゃく",r:"chaku"},
+{k:"使",en:"Use",si:"භාවිත",h:"つかう",r:"tsukau"},
+{k:"作",en:"Make",si:"සෑදීම",h:"つくる",r:"tsukuru"},
+{k:"始",en:"Start",si:"ආරම්භ",h:"はじめる",r:"hajimeru"},
+
+{k:"終",en:"End",si:"අවසාන",h:"おわる",r:"owaru"},
+{k:"開",en:"Open",si:"විවෘත",h:"ひらく",r:"hiraku"},
+{k:"閉",en:"Close",si:"වසන්න",h:"とじる",r:"tojiru"},
+{k:"歩",en:"Walk",si:"ඇවිදින්න",h:"あるく",r:"aruku"},
+{k:"走",en:"Run",si:"දුවන්න",h:"はしる",r:"hashiru"},
+
+{k:"送",en:"Send",si:"යවන්න",h:"おくる",r:"okuru"},
+{k:"返",en:"Return",si:"ආපසු",h:"かえす",r:"kaesu"},
+{k:"借",en:"Borrow",si:"ණය",h:"かりる",r:"kariru"},
+{k:"貸",en:"Lend",si:"දෙන්න",h:"かす",r:"kasu"},
+{k:"取",en:"Take",si:"ගන්න",h:"とる",r:"toru"},
+
+{k:"打",en:"Hit",si:"ගහන්න",h:"うつ",r:"utsu"},
+{k:"投",en:"Throw",si:"විසි",h:"なげる",r:"nageru"},
+{k:"持",en:"Hold",si:"අල්ලන්න",h:"もつ",r:"motsu"},
+{k:"指",en:"Point",si:"පෙන්වන්න",h:"ゆび",r:"yubi"},
+{k:"探",en:"Search",si:"සොයන්න",h:"さがす",r:"sagasu"},
+
+{k:"接",en:"Contact",si:"සම්බන්ධ",h:"せつ",r:"setsu"},
+{k:"放",en:"Release",si:"අතහරින්න",h:"ほう",r:"hou"},
+{k:"数",en:"Number",si:"සංඛ්‍යාව",h:"すう",r:"suu"},
+{k:"新",en:"New",si:"අලුත්",h:"しん",r:"shin"},
+{k:"方",en:"Direction",si:"දිශාව",h:"ほう",r:"hou"},
+
+{k:"旅",en:"Travel",si:"ගමන",h:"りょ",r:"ryo"},
+{k:"族",en:"Family",si:"පවුල",h:"ぞく",r:"zoku"},
+{k:"親",en:"Parent",si:"මව්පිය",h:"しん",r:"shin"},
+{k:"兄",en:"Brother",si:"අයියා",h:"きょう",r:"kyou"},
+{k:"姉",en:"Sister",si:"අක්කා",h:"し",r:"shi"},
+
+{k:"妹",en:"Younger sister",si:"නංගි",h:"まい",r:"mai"},
+{k:"弟",en:"Younger brother",si:"මල්ලි",h:"てい",r:"tei"}
+];
