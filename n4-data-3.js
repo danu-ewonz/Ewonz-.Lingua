2d75105c652c942546fdae88a089e61ce7261635
danu-ewonz <ewonz2006@gmail.com>
2026-06-07 14:09:03 +0530

    Replace kanji list with new structure and entries

--- n4-data-3.js
+++ n4-data-3.js
@@ -1,29 +1,11 @@
-var kanjiN4_3 = [
-{kanji:"銀", kana:"ぎん", romaji:"gin", meaning:"silver"},
-{kanji:"銀行", kana:"ぎんこう", romaji:"ginkou", meaning:"bank"},
-{kanji:"店", kana:"みせ", romaji:"mise", meaning:"shop"},
-{kanji:"市", kana:"し", romaji:"shi", meaning:"city"},
-{kanji:"町", kana:"まち", romaji:"machi", meaning:"town"},
-{kanji:"村", kana:"むら", romaji:"mura", meaning:"village"},
-{kanji:"父", kana:"ちち", romaji:"chichi", meaning:"father"},
-{kanji:"母", kana:"はは", romaji:"haha", meaning:"mother"},
-{kanji:"兄", kana:"あに", romaji:"ani", meaning:"older brother"},
-{kanji:"姉", kana:"あね", romaji:"ane", meaning:"older sister"},
+window.kanjiList = window.kanjiList.concat([
+  {k:"山",en:"Mountain",si:"කන්ද",h:"やま",r:"yama"},
+  {k:"川",en:"River",si:"ගඟ",h:"かわ",r:"kawa"},
+  {k:"海",en:"Sea",si:"මුහුද",h:"うみ",r:"umi"},
+  {k:"空",en:"Sky",si:"අහස",h:"そら",r:"sora"},
+  {k:"花",en:"Flower",si:"මල්",h:"はな",r:"hana"},
 
-{kanji:"弟", kana:"おとうと", romaji:"otouto", meaning:"younger brother"},
-{kanji:"妹", kana:"いもうと", romaji:"imouto", meaning:"younger sister"},
-{kanji:"友", kana:"とも", romaji:"tomo", meaning:"friend"},
-{kanji:"会", kana:"あう", romaji:"au", meaning:"meet"},
-{kanji:"国", kana:"くに", romaji:"kuni", meaning:"country"},
-{kanji:"外", kana:"そと", romaji:"soto", meaning:"outside"},
-{kanji:"内", kana:"うち", romaji:"uchi", meaning:"inside"},
-{kanji:"東", kana:"ひがし", romaji:"higashi", meaning:"east"},
-{kanji:"西", kana:"にし", romaji:"nishi", meaning:"west"},
-{kanji:"南", kana:"みなみ", romaji:"minami", meaning:"south"},
-
-{kanji:"北", kana:"きた", romaji:"kita", meaning:"north"},
-{kanji:"高", kana:"たかい", romaji:"takai", meaning:"tall"},
-{kanji:"低", kana:"ひくい", romaji:"hikui", meaning:"low"},
-{kanji:"多", kana:"おおい", romaji:"ooi", meaning:"many"},
-{kanji:"少", kana:"すくない", romaji:"sukunai", meaning:"few"}
-];
+  {k:"魚",en:"Fish",si:"මාළු",h:"さかな",r:"sakana"},
+  {k:"犬",en:"Dog",si:"බල්ලා",h:"いぬ",r:"inu"},
+  {k:"鳥",en:"Bird",si:"කුරුල්ලා",h:"とり",r:"tori"}
+]);
